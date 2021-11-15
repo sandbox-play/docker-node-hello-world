9bc3f14da0ccd2dc41d00224a82d6f32f02b845a
deepak <yadavdeepak1792@gmail.com>
2021-11-16 02:25:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.end('Hello World\n')
 })
+var apm = require('elastic-apm-node').start({
+                                      serviceName: 'nodeapp',
+                                      serverUrl: 'http://apm-server.logging:8200'
+                                             })
+
 
 server.listen(port)
 
